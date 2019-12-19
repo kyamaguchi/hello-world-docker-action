8c2f67a307f99d059b19bf5bd806b3d34d4e294c
kyamaguchi <kzh.yap@gmail.com>
2019-12-19 09:24:55 +0900

    Check ruby

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,21 @@
 echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
+
+echo 'pwd'
 pwd
+
+echo 'ls -al'
 ls -al
+
+echo 'ls -al vendor'
 ls -al vendor
+
+echo 'cat /etc/alpine-release'
 cat /etc/alpine-release
+
+echo 'which ruby'
+which ruby
+
+echo 'ruby -v'
+ruby -v
